b242e7976ca95a2b530006d78cb39fd4e42d4aad
cranky_sysadmin <kaleb_palmby@yahoo.com>
2025-06-17 14:44:18 -0400

    AZ Automation account automation

---
+++ AzAutomateAccounts - Import Python Packages.ps1
@@ -0,0 +1,62 @@
+#import Python Packages into Azure Automation and its dependancies
+Connect-AzAccount
+
+$resourcegroup = read-host: "Resource Group name: "
+$automationname = read-host: "Automation Account Name: "
+
+
+$input = Read-Host = "What Python Package to Insert: "
+
+
+$packageurl = "https://pypi.org/pypi/$input/json"
+
+$response = Invoke-RestMethod -Uri $pypiUrl
+
+
+
+$wheelUrls = $response.urls | Where-Object { $_.url -like "*.whl" } | Select-Object -ExpandProperty url
+$libraries = @()
+
+if($wheelUrls -ne $null){
+mkdir C:\python_temp -erroraction silentlycontinue
+cd C:\python_temp
+pip download $input -d wheel_libraries
+
+$wheelpackages = get-childitem C:\python_temp\wheel_libraries
+$wh = $wheelpackages | select name 
+
+foreach ($package in $wh)
+{
+    $ba = $package -split "-" 
+    $basename = ($ba -split "`n")[0] -replace '^@{Name=', ''
+    $packageurl = "https://pypi.org/pypi/$basename/json"
+    $response = Invoke-RestMethod -Uri $packageurl
+
+
+
+    $wheelUrls = $response.urls | Where-Object { $_.url -like "*.whl" } | Select-Object -ExpandProperty url 
+    $wheelUrls
+    if ($wheelurls.count -gt 1)
+    {
+        $pac = $wheelUrls | where {$_ -like "*win_amd64*"} | select -first 1
+        $libraries += $pac
+    }
+
+    else
+    {
+        $libraries += $wheelUrls
+    }
+}
+}
+
+foreach ($lib in $libraries)
+{
+    $filename = [System.IO.Path]::getfilenamewithoutextension($lib)
+    $package = $filename -split "-" | select -first 1
+
+   New-AzAutomationPython3Package -automationaccountname $automationname -name $package -ResourceGroupName $resourcegroup -ContentLinkUri $lib
+}
+
+cd C:\
+del C:\python_temp -force -recurse -Confirm:$false
+
