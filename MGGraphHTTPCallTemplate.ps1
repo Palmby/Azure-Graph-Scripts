8a58202657d6582239bd2ce9287e0e3bf5ac2067
cranky_sysadmin <kaleb_palmby@yahoo.com>
2026-09-21 09:00:26 -0500

    MGGraphHTTPTemplate

---
+++ MGGraphHTTPCallTemplate.ps1
@@ -0,0 +1,58 @@
+function getmggraphtoken {
+
+param(
+[parameter(Mandatory=$true)]
+[string]$ClientID,
+[parameter(Mandatory=$true)]
+[string]$TenantID,
+[parameter(Mandatory=$true)]
+[string]$client_secret
+
+)
+
+
+$Scope = "https://graph.microsoft.com/.default"
+
+$url = 'https://login.microsoftonline.com/' + $tenantId + '/oauth2/v2.0/token'
+
+
+$body = @{
+    grant_type = "client_credentials"
+    client_id = $ClientID
+    client_secret = $client_secret
+    scope = "https://graph.microsoft.com/.default"
+}
+
+$tokenRequest = Invoke-WebRequest -Method Post -Uri $url -ContentType "application/x-www-form-urlencoded" -Body $body
+
+$token = ($tokenRequest.Content | ConvertFrom-Json).access_token
+
+return $token
+}
+
+
+function MGAPICall {
+    param (
+        [parameter(Mandatory=$true)]
+        [string]$uri,
+        [parameter(Mandatory=$true)]
+        [string]$access_token,
+        [parameter(Mandatory=$true)]
+        [string]$method
+    )
+
+
+    $headers = @{
+    Authorization = "Bearer $access_Token"
+    }
+
+    $response = Invoke-RestMethod -Method $method -uri $uri -headers $headers
+
+    return $response.value
+}
+
+
+$token = getmggraphtoken -clientID  -TenantID -client_secret 
+
+MGAPICall -access_token $token -method  -uri 
+
